14a2f722e343c36e6c58d1c743b473e7b02fae2f
iqbalatma <iqbalatma@gmail.com>
2022-08-14 21:17:01 +0700

    add datetime

--- app/Http/Controllers/API/CheckinController.php
+++ app/Http/Controllers/API/CheckinController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ticket;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CheckinController extends Controller
@@ -12,24 +13,30 @@ class CheckinController extends Controller
   {
     $code = $request->all()['code'];
     $ticket = Ticket::where('code', $code)->first();
+    $mytime = Carbon::now();
+    $mytime->toDateTimeString();
 
     if ($ticket) {
       if ($ticket->checkin_status) {
         return response()->json([
           "message" => "Ticket is already checkin !",
-          "status" => 403
+          "status" => 403,
+          "timestamp" => $mytime
         ])->setStatusCode(403);;
       }
 
       Ticket::where('code', $code)->update(['checkin_status' => '1']);
       return response()->json([
         "message" => "Checkin successfuly !",
-        "status" => 200
+        "status" => 200,
+        "timestamp" => $mytime
+
       ])->setStatusCode(200);;
     } else {
       return response()->json([
         "message" => "Code is invalid !",
-        "status" => 404
+        "status" => 404,
+        "timestamp" => $mytime
       ])->setStatusCode(404);
     }
   }
